e883d54af1df26e4461f1bd21e340abfaf469f10
IzumiShaka-desu <shakaaji29@gmail.com>
2021-07-16 10:06:12 +0700

    fix : type casting error

--- lib/permissions_plugin.dart
+++ lib/permissions_plugin.dart
@@ -29,60 +29,65 @@ enum Permission {
   WRITE_EXTERNAL_STORAGE
 }
 
-enum PermissionState {
-  GRANTED,
-  DENIED,
-  UNKNOWN
-}
+enum PermissionState { GRANTED, DENIED, UNKNOWN }
 
 class PermissionsPlugin {
-
   static const MethodChannel _channel =
       const MethodChannel('permissions_plugin');
 
   static const List<String> CommandName = [
-    "request-permissions", 
-    "check-permissions", 
+    "request-permissions",
+    "check-permissions",
     "check-battery-optimization",
-    "request-battery-optimization", 
+    "request-battery-optimization",
   ];
 
-  static Future<Map<Permission, PermissionState>> requestPermissions(List<Permission> permissions) async {
-    if(permissions.isEmpty) return {};
+  static Future<Map<Permission, PermissionState>> requestPermissions(
+      List<Permission> permissions) async {
+    if (permissions.isEmpty) return {};
 
     final List<String> permissionsValues = permissions
         .map((Permission pg) => pg.toString().split(".")[1])
-        .cast<String>().toList();
+        .cast<String>()
+        .toList();
 
-    final Map<dynamic, dynamic> results = await (_channel.invokeMethod(CommandName[0], permissionsValues) as FutureOr<Map<dynamic, dynamic>>);
-    final Map<Permission, PermissionState> permissionResult = _mappingResult(Map<int, int>.from(results));
+    final Map<dynamic, dynamic>? results =
+        await (_channel.invokeMethod<Map<dynamic, dynamic>>(
+            CommandName[0], permissionsValues));
+    final Map<Permission, PermissionState> permissionResult =
+        _mappingResult(Map<int, int>.from(results ?? {}));
     return permissionResult;
   }
 
-  static Future<Map<Permission, PermissionState>> checkPermissions(List<Permission> permissions) async {
-    if(permissions.isEmpty) return {};
+  static Future<Map<Permission, PermissionState>> checkPermissions(
+      List<Permission> permissions) async {
+    if (permissions.isEmpty) return {};
 
     final List<String> permissionsValues = permissions
         .map((Permission pg) => pg.toString().split(".")[1])
-        .cast<String>().toList();
+        .cast<String>()
+        .toList();
 
-    final Map<dynamic, dynamic> results = await (_channel.invokeMethod(CommandName[1], permissionsValues) as FutureOr<Map<dynamic, dynamic>>);
-    final Map<Permission, PermissionState> permissionResult = _mappingResult(Map<int, int>.from(results));
+    final Map<dynamic, dynamic> results =
+        await (_channel.invokeMethod(CommandName[1], permissionsValues));
+    final Map<Permission, PermissionState> permissionResult =
+        _mappingResult(Map<int, int>.from(results));
     return permissionResult;
   }
 
   static Future<PermissionState> get isIgnoreBatteryOptimization async {
-    final int result = await (_channel.invokeMethod(CommandName[2]) as FutureOr<int>);
-    return PermissionState.values[result];
+    final int? result = await (_channel.invokeMethod<int>(CommandName[2]));
+    return PermissionState.values[result ?? 2];
   }
 
   static Future<PermissionState> get requestIgnoreBatteryOptimization async {
-    final int result = await (_channel.invokeMethod(CommandName[3]) as FutureOr<int>);
-    return PermissionState.values[result];
+    final int? result = await (_channel.invokeMethod<int>(CommandName[3]));
+    return PermissionState.values[result ?? 2];
   }
 
-  static Map<Permission, PermissionState> _mappingResult(Map<int, int> results){
-    return results.map((int pg, int pr) => MapEntry(Permission.values[pg], PermissionState.values[pr]));
+  static Map<Permission, PermissionState> _mappingResult(
+      Map<int, int> results) {
+    return results.map((int pg, int pr) =>
+        MapEntry(Permission.values[pg], PermissionState.values[pr]));
   }
-
 }
